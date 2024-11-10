f204695b3be4b97876042a3cd32dc933b7075dde
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-10 22:42:35 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -1,11 +1,11 @@
 import motor.motor_asyncio
-from info import AUTH_CHANNEL, DATABASE_URI
+from info import AUTH_CHANNEL, OTHER_DB_URI
 
 class JoinReqs:
 
     def __init__(self):
-        if DATABASE_URI:
-            self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
+        if OTHER_DB_URI:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(OTHER_DB_URI)
             self.db = self.client["JoinReqs"]
             self.col = self.db[str(AUTH_CHANNEL)]
         else:
