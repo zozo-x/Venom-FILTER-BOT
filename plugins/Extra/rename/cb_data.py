c1111b6cbf8421bca9f9883de8ff3ed17ca59a19
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-10 22:16:53 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -32,7 +32,7 @@ async def rename(bot, update):
 	#await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
 	#reply_to_message_id=update.message.reply_to_message.id,  
 	#reply_markup=ForceReply(True))
-        await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__")
+        await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__", reply_to_message_id=update.message.reply_to_message.id)
         msg = await bot.listen(update.from_user.id)
         await refunc(bot, update, msg.text, update.message.reply_to_message)
 	
