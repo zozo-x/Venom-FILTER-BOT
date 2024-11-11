919ec46b9236718aa6ef25ece8549eb1407fe2ef
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-11 20:00:16 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -17,25 +17,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@Client.on_callback_query(filters.regex('cancel'))
-async def cancel(bot,update):
-	try:
-           await update.message.delete()
-	except:
-           return
 
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot, update):
-	user_id = update.message.chat.id
-	date = update.message.date
-	await update.message.delete()
-	#await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
-	#reply_to_message_id=update.message.reply_to_message.id,  
-	#reply_markup=ForceReply(True))
-        await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__", reply_to_message_id=update.message.reply_to_message.id)
-        msg = await bot.listen(update.from_user.id)
-        await refunc(bot, update, msg.text, update.message.reply_to_message)
-	
 
 @Client.on_callback_query(filters.regex("upload"))
 async def doc(bot, update):
