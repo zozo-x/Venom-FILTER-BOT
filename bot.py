8e5a4101e5d8c28a9cc1d335c145770b4ab58a04
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-11 20:59:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,18 +8,6 @@ import sys, glob, importlib, logging, logging.config, pytz, asyncio
 from pathlib import Path
 
 # Get logging configurations
-#logging.config.fileConfig('logging.conf')
-#logging.getLogger().setLevel(logging.INFO)
-#logging.getLogger("pyrogram").setLevel(logging.ERROR)
-#logging.getLogger("imdbpy").setLevel(logging.ERROR)
-#logging.basicConfig(
-#    level=logging.INFO,
-#    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-#)
-#logging.getLogger("aiohttp").setLevel(logging.ERROR)
-#logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
-
-# Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
