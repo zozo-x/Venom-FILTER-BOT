405f894423000449a9a5de5639c0c9f0cb8bddf3
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-13 17:02:31 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -24,19 +24,6 @@ async def cancel(bot,update):
     except:
         return
 
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot, update):
-    user_id = update.message.chat.id
-    date = update.message.date
-    await update.message.delete()
-	#await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
-	#reply_to_message_id=update.message.reply_to_message.id,  
-	#reply_markup=ForceReply(True))
-    await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__", reply_to_message_id=update.message.reply_to_message.id)
-    msg = await bot.listen(update.from_user.id)
-    await refunc(bot, update, msg.text, update.message.reply_to_message)
-	
-
 @Client.on_callback_query(filters.regex("upload"))
 async def doc(bot, update):
     try:
