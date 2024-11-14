af2a85df8fb41e8bdb2bf3181c6962e972ba5c1a
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-14 16:50:52 +0530

    Update files_delete.py

--- plugins/files_delete.py
+++ plugins/files_delete.py
@@ -5,7 +5,7 @@
 import re, logging
 from pyrogram import Client, filters
 from info import DELETE_CHANNELS
-from database.ia_filterdb import Media, unpack_new_file_id
+from database.ia_filterdb import col, sec_col, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
 media_filter = filters.document | filters.video | filters.audio
@@ -23,14 +23,14 @@ async def deletemultiplemedia(bot, message):
 
     file_id, file_ref = unpack_new_file_id(media.file_id)
 
-    result = await Media.collection.delete_one({
+    result = await col.delete_one({
         '_id': file_id,
     })
     if result.deleted_count:
         logger.info('File is successfully deleted from database.')
     else:
         file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-        result = await Media.collection.delete_many({
+        result = await col.delete_many({
             'file_name': file_name,
             'file_size': media.file_size,
             'mime_type': media.mime_type
@@ -38,7 +38,7 @@ async def deletemultiplemedia(bot, message):
         if result.deleted_count:
             logger.info('File is successfully deleted from database.')
         else:
-            result = await Media.collection.delete_many({
+            result = await col.delete_many({
                 'file_name': media.file_name,
                 'file_size': media.file_size,
                 'mime_type': media.mime_type
