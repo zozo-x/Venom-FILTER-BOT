1df51d195c4420ff143817241af81b195b62bb66
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-18 20:48:19 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -286,7 +286,7 @@ async def auto_approve(client, message: ChatJoinRequest):
                     reply_markup=InlineKeyboardMarkup(button)
                 )
                 btn = [[
-                    InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
+                    InlineKeyboardButton("Get File Again", callback_data=f'del#{file_id}')
                 ]]
                 k = await client.send_message(message.from_user.id,"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
                 await asyncio.sleep(600)
@@ -346,7 +346,7 @@ async def auto_approve(client, message: ChatJoinRequest):
             reply_markup=InlineKeyboardMarkup(button)
         )
         btn = [[
-            InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
+            InlineKeyboardButton("Get File Again", callback_data=f'del#{file_id}')
         ]]
         k = await client.send_message(message.from_user.id, "<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
         await asyncio.sleep(600)
