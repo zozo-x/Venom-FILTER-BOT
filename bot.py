7acaa5c18b2fbc10e61e665fcef63f19a598c8d1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-22 16:13:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,6 @@ async def start():
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    logging.info(LOG_STR)
     logging.info(script.LOGO)
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
