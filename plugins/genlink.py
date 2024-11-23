362f027859d5b2c388ae1884b1376df7640bb29f
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-23 13:06:32 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -113,4 +113,6 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{post}")
+    string = post.document
+    b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{b_64}")
