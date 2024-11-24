00f0db5d96928f7a2625a23f5198ed7f1fe05938
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-24 11:35:04 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -41,10 +41,19 @@ async def save_file(media):
         'file_size': media.file_size,
         'caption': media.caption.html if media.caption else None
     }
+    found1 = {'file_name': file_name}
+    found = {'file_id': file_id}
+    check1 = col.find_one(found1)
+    if check1:
+        print(f"{file_name} is already saved.")
+        return False, 0
+    check = col.find_one(found)
+    if check:
+        print(f"{file_name} is already saved.")
+        return False, 0
     if MULTIPLE_DATABASE == True:
-        found1 = {'file_name': file_name}
-        check1 = col.find_one(found1)
-        if check1:
+        check3 = sec_col.find_one(found)
+        if check3:
             print(f"{file_name} is already saved.")
             return False, 0
         check2 = sec_col.find_one(found1)
@@ -54,19 +63,13 @@ async def save_file(media):
         result = db.command('dbstats')
         data_size = result['dataSize']
         if data_size > 503316480:
-            found = {'file_id': file_id}
-            check = col.find_one(found)
-            if check:
+            try:
+                sec_col.insert_one(file)
+                print(f"{file_name} is successfully saved.")
+                return True, 1
+            except DuplicateKeyError:      
                 print(f"{file_name} is already saved.")
                 return False, 0
-            else:
-                try:
-                    sec_col.insert_one(file)
-                    print(f"{file_name} is successfully saved.")
-                    return True, 1
-                except DuplicateKeyError:      
-                    print(f"{file_name} is already saved.")
-                    return False, 0
         else:
             try:
                 col.insert_one(file)
