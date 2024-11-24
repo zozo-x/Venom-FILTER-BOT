50036e761e1bb34e04b6c6db16a949561a79475e
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-11-24 14:56:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,13 @@ from pathlib import Path
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client, idle
 from database.users_chats_db import db
