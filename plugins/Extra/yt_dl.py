eb046b7e02b0938be462ec901d52f302a34f0a62
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-06 12:19:23 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os, requests, asyncio, math, time, wget
 from pyrogram import filters, Client
 from pyrogram.types import Message
-
+from info import CHNL_LNK
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
