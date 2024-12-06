09d9c962935e51c52ded5f67a7508af1877a87b2
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-06 12:03:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -639,7 +639,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                             InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                         ],[
-                            InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=OWNER_LINK)
+                            InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=OWNER_LNK)
                         ]]
                     )
                 )
